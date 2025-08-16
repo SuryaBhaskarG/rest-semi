a6a2fe19458fec07db161da4823c2d7632302a71
SuryaBhaskarG <127416681+SuryaBhaskarG@users.noreply.github.com>
2025-08-16 11:57:46 +0530

    Update UserRepositoryTest.java

--- User-App/src/test/java/com/surya/example/repositories/UserRepositoryTest.java
+++ User-App/src/test/java/com/surya/example/repositories/UserRepositoryTest.java
@@ -2,6 +2,8 @@ package com.surya.example.repositories;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,35 @@ import com.surya.example.entities.User;
 
 	        Assertions.assertNotNull(saved.getId());
 	    }
+	    
+	    
+	    
+	    
+	    
+	    
+	    @Test
+	    void findByNameTest() {
+	        User user = new User(null, "Surya", "surya@gmail.com");
+	        userRepository.save(user);
+
+	        List<User> found = userRepository.findByName("Surya");
+
+	        assertEquals(1, found.size());
+	    }
+
+	    @Test
+	    void findByNameIgnoreCaseTest() {
+	        User user = new User(null, "Surya", "surya@gmail.com");
+	        userRepository.save(user);
+
+	        List<User> found = userRepository.findByNameIgnoreCase("surya");
+
+	        assertEquals(1, found.size());
+	    }
+	    
+	    
+	    
+	    
 	}
 
 	
